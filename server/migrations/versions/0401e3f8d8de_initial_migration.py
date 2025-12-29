7ad7dfeb0e53bd9c86e9447aaceeb3bc65c06c13
slaukoter <slaukoter@gmail.com>
2025-12-29 15:47:25 -0600

    passes tests

--- server/migrations/versions/0401e3f8d8de_initial_migration.py
+++ server/migrations/versions/0401e3f8d8de_initial_migration.py
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('users',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(), nullable=False),
-    sa.Column('_password_hash', sa.String(), nullable=False),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.Column('image_url', sa.String(), nullable=True),
     sa.Column('bio', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
